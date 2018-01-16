48df231d92c24af971746a1ca685e52b3577646a
amals10 <amalsom10@gmail.com>
2018-01-16 23:42:54 +0000

    Finished

---
+++ Testcase1.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+echo " Script for generating work report"
+echo "Enna thudakiyallo :[y/n]"
+read c
+if (("$c"=="y")); then
+echo "Lets being Bro.."
+echo "Enter the date"
+read date 
+touch /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "#######\nDate : "$date"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "Shift time :"
+read shtime
+echo -e "Shift Time : "$shtime"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "Members in Shift :"
+read member
+echo -e "Members in Shift : "$member"\n#######\n" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo "No of ticket attempted :"
+read n
+if (("$n"=="0")); then
+echo -e " Aahaa kollallo today ticket onum illa "
+break
+else
+echo -e "\n-----------\nTickets:\n-----------" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+if (("$n">="5")); then
+echo "Today killi poyi alla....."
+fi
+for ((i=1 ; i<="$n" ; i++)) 
+do
+echo -e "-----------\n"$i"#" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "-----------"
+echo  "Ticket ID  : "
+read tid
+echo -e "Ticket ID   : "$tid"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo  "Subject    : "
+read sub
+echo -e "Subject     : "$sub"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo  "Customer   : "
+read cust
+echo -e "Customer    :"$cust"" >>  /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo  "Description : "
+read descri
+echo -e "Description :"$descri"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "-----------"
+done
+fi
+echo -e " Lets being with chat "
+echo "No of chat attempted :"
+read c
+if (("$c"=="0")); then
+echo -e "Aarum chatan vanilla"
+break
+else
+echo -e "\n-----------\nChats:\n-----------" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+for ((j=1 ; j<="$c" ; j++)) 
+do
+echo -e "-----------\n"$j"#" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "-----------"
+echo  "Customer Name :"
+read cname
+echo -e "Customer Name : "$cname"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo  "Email    : "
+read mail
+echo -e "Email         : "$mail"" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo  "Description   : "
+read des
+echo -e "Description   :"$des"" >>  /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+echo -e "-----------"
+echo -e "-----------" >> /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+done
+fi
+
+echo " Completed Bro......"
+echo -e "Did you wish to open the report"
+read open
+if (("$open"=="y")); then
+vim /hdin/techies/hdinamalso/testsh/sh/"$date".txt
+else
+exit
+fi
+else
+echo "Quiting........Good bye Bro"
+fi
+
---
+++ completed.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+echo " Script for generating work report"
+echo "Enna thudakiyallo :[y/n]"
+read c
+if (("$c"=="y")); then
+echo "Lets being Bro.."
+echo "Enter the date"
+read date
+touch /home/amal/Desktop/latest"$date".txt
+echo -e "#######\nDate : "$date"" >> /home/amal/Desktop/latest/"$date".txt
+echo -e "Shift time :"
+read shtime
+echo -e "Shift Time : "$shtime"" >> /home/amal/Desktop/latest/"$date".txt
+echo -e "Members in Shift :"
+read member
+echo -e "Members in Shift : "$member"\n#######\n" >> /home/amal/Desktop/latest/"$date".txt
+echo "No of ticket attempted :"
+read num
+num=$((num-1))
+if (("$num"=="0")); then
+echo -e " Aahaa kollallo today ticket onum illa "
+break
+else
+echo -e "\n-----------\nTickets:\n-----------" >> /home/amal/Desktop/latest/"$date".txt
+touch /home/amal/Desktop/latest/tmp
+cat /home/amal/Desktop/latest/Metrics\ DimeNOC\ Administrator.csv | cut -d '"' -f6 | tail -n +2 | cat >> /home/amal/Desktop/latest/tmp
+j=1
+for ((i=0 ; i<="$num" ; i++)) 
+do
+echo -e "-----------\n"$i"#" >> /home/amal/Desktop/latest/"$date".txt
+echo -e "-----------"
+id=($(cat /home/amal/Desktop/latest/Metrics\ DimeNOC\ Administrator.csv  | cut -d '"' -f4| tail -n +2))
+echo -e "Ticket ID   : "${id[i]}"" >> /home/amal/Desktop/latest/"$date".txt
+line=($(sed -n ${j}p /home/amal/Desktop/latest/tmp))
+echo -e "Subject   : "${line[*]}"" >> /home/amal/Desktop/latest/"$date".txt
+unset line 
+j=$((j+1))
+echo  "Customer name of "${id[i]}"  : "
+read cust
+echo -e "Customer    :"$cust"" >>  /home/amal/Desktop/latest/"$date".txt
+echo  "Description name of "${id[i]}" : "
+read descri
+echo -e "Description :"$descri"" >> /home/amal/Desktop/latest/"$date".txt
+echo -e "-----------"
+done 
+rm /home/amal/Desktop/latest/tmp
+fi
+echo -e " Lets being with chat "
+echo "No of chat attempted :"
+read chat
+if (("$chat"=="0")); then
+echo -e "Aarum chatan vanilla"
+break
+else
+cat Metrics\ DimeNOC\ Administrator_chat.csv | cut -d '"' -f6 | tail -n +2 | cat >> /home/amal/Desktop/latest/tmp1
+#cat Metrics\ DimeNOC\ Administrator_chat.csv | cut -d '"' -f18 | tail -n +2 | cat >> /home/amal/Desktop/latest/tmp2
+echo -e "\n-----------\nChats:\n-----------" >> /home/amal/Desktop/latest/"$date".txt
+for ((k=1 ; k<="$chat" ; k++)) 
+do
+echo -e "-----------\n"$k"#" >> /home/amal/Desktop/latest/"$date".txt
+echo -e "-----------"
+name=($(sed -n ${k}p /home/amal/Desktop/latest/tmp1))
+echo -e "Customer Name : "${name[*]}"" >> /home/amal/Desktop/latest/"$date".txt
+echo  "Email of "${name[*]}"   : "
+read mail
+echo -e "Email         : "$mail"" >> /home/amal/Desktop/latest/"$date".txt
+echo  "Description of "${name[*]}"   : "
+read des
+echo -e "Description   :"$des"" >>  /home/amal/Desktop/latest/"$date".txt
+echo -e "-----------"
+echo -e "-----------" >> /home/amal/Desktop/latest/"$date".txt
+done 
+rm /home/amal/Desktop/latest/tmp1
+fi
+
+echo " Completed Bro......"
+echo -e "Did you wish to open the report"
+read open
+if (("$open"=="y")); then
+vim /home/amal/Desktop/latest/"$date".txt
+else
+exit
+fi
+else
+echo "Quiting........Good bye Bro"
+fi
+
+
+
+
